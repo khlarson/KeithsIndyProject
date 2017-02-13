f36bb0df7658d0a31ef4ef014348527cd6f4edac
Keith <khlarson@madisoncollege.edu>
2017-02-13 16:16:06 -0600

    updated Location code

--- src/main/java/edu/matc/entity/Sponsor.java
+++ src/main/java/edu/matc/entity/Sponsor.java
@@ -1,7 +1,120 @@
 package edu.matc.entity;
 
+import edu.matc.util.LocalDateAttributeConverter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.Period;
+
+import static java.time.LocalDate.now;
+
 /**
- * Created by student on 2/13/17.
+ * A class to represent a location.
+ *
+ * @author //khlarson
  */
+@Entity
+@Table(name = "Sponsor")
 public class Sponsor {
+    @Column(name = "sponsor_Name")
+    private String name;
+
+    @Column(name = "websiteURL")
+    private String websiteURL;
+
+    @Column(name = "description")
+    private String description;
+
+    @Id
+    @GeneratedValue(generator="increment")
+    @GenericGenerator(name="increment", strategy = "increment")
+    @Column(name = "id")
+    private int sponsorID;
+
+    /**
+     * Instantiates a new Location.
+     */
+    public Sponsor() {
+    }
+
+    public Sponsor(String name, String websiteURL, String description, int sponsorID) {
+        this.name = name;
+        this.websiteURL = websiteURL;
+        this.description = description;
+        this.sponsorID = sponsorID;
+    }
+
+    /**
+     * Gets location name.
+     *
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets location name.
+     *
+     * @param name the first name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Gets websiteURL.
+     *
+     * @return the websiteURL
+     */
+    public String getWebsiteURL() {
+        return websiteURL;
+    }
+
+    /**
+     * Sets websiteURL.
+     *
+     * @param websiteURL the websiteURL
+     */
+    public void setWebsiteURL(String websiteURL) {
+        this.websiteURL = websiteURL;
+    }
+
+    /**
+     * Gets description.
+     *
+     * @return the description
+     */
+
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Sets description.
+     *
+     * @param description the description
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * Gets sponsorID.
+     *
+     * @return the sponsorID
+     */
+    public int getSponsorID() {
+        return sponsorID;
+    }
+
+    /**
+     * Sets sponsorID.
+     *
+     * @param sponsorID the sponsorID
+     */
+    public void setSponsorID(int sponsorID) {
+        this.sponsorID = sponsorID;
+    }
 }
--- src/main/java/edu/matc/persistence/SponsorDAO.java
+++ src/main/java/edu/matc/persistence/SponsorDAO.java
@@ -1,7 +1,123 @@
 package edu.matc.persistence;
 
+import edu.matc.entity.Sponsor;
+import edu.matc.entity.User;
+import org.apache.log4j.Logger;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created by student on 2/13/17.
+ * Created by khlarson on 2/2/16.
  */
 public class SponsorDAO {
+
+    private final Logger log = Logger.getLogger(this.getClass());
+
+    /** Return a list of all sponsors
+     *
+     * @return All sponsors
+     */
+    public List<Sponsor> getAllSponsors() {
+        List<Sponsor> sponsors = new ArrayList<Sponsor>();
+        Session session = SessionFactoryProvider.getSessionFactory().openSession();
+        sponsors = session.createCriteria(User.class).list();
+        session.close();
+        return sponsors;
+    }
+
+    /**
+     * retrieve a user given their id
+     *
+     * @param id the user's id
+     * @return user
+     */
+    public Sponsor getSponsor(int id) {
+        //add try catch HibernateException he and e + log
+        Session session = SessionFactoryProvider.getSessionFactory().openSession();
+        Sponsor sponsor = (Sponsor) session.get(Sponsor.class, id);
+        session.close();
+        return sponsor;
+    }
+
+    /**
+     * add a sponsor
+     *
+     * @param sponsor
+     * @return the id of the inserted record
+     */
+    public int addSponsor(Sponsor sponsor) {
+        int id = 0 ;
+        Session session = SessionFactoryProvider.getSessionFactory().openSession() ;
+        Transaction tx = null ;
+        try {
+            tx = session.beginTransaction();
+            id = (int) session.save(sponsor);
+            tx.commit();
+        }
+        catch (HibernateException e) {
+            if (null!=tx) {
+                tx.rollback() ;
+            }
+            log.error("HibernateException:" + e);
+        }
+        finally {
+            session.close() ;
+        }
+        return id ;
+    }
+
+    /**
+     * delete a user by id
+     * @param id the user's id
+     * @return the id of the deleted record
+     */
+    public void deleteSponsor(int id) {
+        Session session = SessionFactoryProvider.getSessionFactory().openSession();
+        Sponsor sponsor = (Sponsor) session.get(Sponsor.class, id);
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            session.delete(sponsor);
+            tx.commit();
+        }
+        catch (HibernateException e) {
+            if (null!=tx) {
+                tx.rollback() ;
+            }
+            log.error("HibernateException:" + e);
+        }
+        finally {
+            session.close() ;
+        }
+    }
+
+    /**
+     * Update the user
+     * @param sponsor
+     */
+    public void updateSponsor(Sponsor sponsor) {
+        Session session = SessionFactoryProvider.getSessionFactory().openSession() ;
+        Transaction tx = null ;
+        try {
+            tx = session.beginTransaction() ;
+            session.update(sponsor) ;
+            tx.commit() ;
+        }
+        catch (HibernateException e) {
+            if (null != tx) {
+                tx.rollback() ;
+            }
+            log.error("HibernateException:" + e);
+        }
+        finally {
+            session.close() ;
+        }
+    }
+
+
+
 }
