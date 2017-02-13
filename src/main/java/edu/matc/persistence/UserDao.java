51fd0a17171fb5de7dc2747e4d8ae2c7b68bd132
Keith <khlarson@madisoncollege.edu>
2017-02-13 14:53:04 -0600

    adding some code

---
+++ src/main/java/edu/matc/persistence/UserDao.java
@@ -0,0 +1,123 @@
+package edu.matc.persistence;
+
+import edu.matc.entity.User;
+import org.apache.log4j.Logger;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by khlarson on 2/2/16.
+ */
+public class UserDao {
+
+    private final Logger log = Logger.getLogger(this.getClass());
+
+    /** Return a list of all users
+     *
+     * @return All users
+     */
+    public List<User> getAllUsers() {
+        //add try catch HibernateException he and e + log
+        List<User> users = new ArrayList<User>();
+        Session session = SessionFactoryProvider.getSessionFactory().openSession();
+        users = session.createCriteria(User.class).list();
+        session.close();
+        return users;
+    }
+
+    /**
+     * retrieve a user given their id
+     *
+     * @param id the user's id
+     * @return user
+     */
+    public User getUser(int id) {
+        //add try catch HibernateException he and e + log
+        Session session = SessionFactoryProvider.getSessionFactory().openSession();
+        User user = (User) session.get(User.class, id);
+        session.close();
+        return user;
+    }
+
+    /**
+     * add a user
+     *
+     * @param user
+     * @return the id of the inserted record
+     */
+    public int addUser(User user) {
+        int id = 0 ;
+        Session session = SessionFactoryProvider.getSessionFactory().openSession() ;
+        Transaction tx = null ;
+        try {
+            tx = session.beginTransaction();
+            id = (int) session.save(user);
+            tx.commit();
+        }
+        catch (HibernateException e) {
+            if (null!=tx) {
+                tx.rollback() ;
+            }
+            log.error("HibernateException:" + e);
+        }
+        finally {
+            session.close() ;
+        }
+        return id ;
+    }
+
+    /**
+     * delete a user by id
+     * @param id the user's id
+     * @return the id of the deleted record
+     */
+    public void deleteUser(int id) {
+        Session session = SessionFactoryProvider.getSessionFactory().openSession();
+        User user = (User) session.get(User.class, id);
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            session.delete(user);
+            tx.commit();
+        }
+        catch (HibernateException e) {
+            if (null!=tx) {
+                tx.rollback() ;
+            }
+            log.error("HibernateException:" + e);
+        }
+        finally {
+            session.close() ;
+        }
+    }
+
+    /**
+     * Update the user
+     * @param user
+     */
+    public void updateUser(User user) {
+        Session session = SessionFactoryProvider.getSessionFactory().openSession() ;
+        Transaction tx = null ;
+        try {
+            tx = session.beginTransaction() ;
+            session.update(user) ;
+            tx.commit() ;
+        }
+        catch (HibernateException e) {
+            if (null != tx) {
+                tx.rollback() ;
+            }
+            log.error("HibernateException:" + e);
+        }
+        finally {
+            session.close() ;
+        }
+    }
+
+
+
+}
---
+++ src/test/java/edu/matc/persistence/UserDaoTest.java
@@ -0,0 +1,76 @@
+package edu.matc.persistence;
+
+import edu.matc.entity.User;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created on 9/13/16.
+ *
+ * @author khlarson
+ */
+public class UserDaoTest {
+
+    UserDao dao;
+    User user;
+    @Before
+    public void setup() {
+        dao = new UserDao();
+
+        user = new User("test", "user", 5, getLocalDate("1993-04-11"));
+    }
+
+    @Test
+    public void getAllUsers() throws Exception {
+        List<User> users = dao.getAllUsers();
+        assertTrue(users.size() > 0);
+    }
+
+    @Test
+    public void getUser() throws Exception {
+        int userid = user.getUserid();
+
+        assertTrue(userid == 5);
+    }
+
+    @Test
+    public void addUser() throws Exception {
+        int id = dao.addUser(user);
+
+        assertNotEquals(0, id);
+        User insertedUser = dao.getUser(id);
+        assertEquals(insertedUser.getFirstName(), user.getFirstName());
+    }
+
+
+    @Test
+    public void deleteUser() throws Exception {
+        int id = dao.addUser(user);
+        dao.deleteUser(id);
+        User deletedUser = dao.getUser(id);
+        assertNull(deletedUser);
+    }
+
+    @Test
+    public void updateUser() throws Exception {
+        int id = dao.addUser(user);
+        String nameOne = user.getFirstName();
+        dao.updateUser(user);
+        String nameTwo = user.getFirstName();
+
+        assertNotEquals(nameOne, nameTwo);
+    }
+
+
+    private LocalDate getLocalDate(String inputDate) {
+        LocalDate date = LocalDate.parse(inputDate) ;
+        return date ;
+    }
+
+}
