0af1ce55c93f2706a9137ffb1a48c696fa9df488
Keith <khlarson@madisoncollege.edu>
2017-02-28 11:57:49 -0600

    Initial API test code added, still needs work

---
+++ src/test/java/edu/matc/api/TestServiceClient.java
@@ -0,0 +1,26 @@
+package edu.matc.api;
+import org.junit.Test;
+import javax.ws.rs.client.*;
+import javax.ws.rs.core.MediaType;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Khlarson on 2/28/17.
+ */
+public class TestServiceClient {
+
+    @Test
+    public void testGoogleApiJSON() throws Exception {
+        //Gives ability to make a  call to an external service
+        Client client = ClientBuilder.newClient();
+        WebTarget target =
+
+                //TODO get target (URI + resource + parameters) from google places api
+                client.target("http://maps.googleapis.com/maps/api/geocode/json?" +
+                        "address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&sensor=false");
+
+        //TODO MediaType... This needs to be changed to what the web service will respond in, either xml or json
+        String response = target.request(MediaType.APPLICATION_JSON).get(String.class);
+        assertEquals("???", response);
+    }
+}
