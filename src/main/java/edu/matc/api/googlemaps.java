7439bb9170c0919967a9b11eb02b4ae95c6d8d4b
Keith <khlarson@madisoncollege.edu>
2017-04-29 14:11:45 -0500

    api key

--- src/main/java/edu/matc/api/googlemaps.java
+++ src/main/java/edu/matc/api/googlemaps.java
@@ -2,27 +2,65 @@ package edu.matc.api;
 
 import edu.matc.entity.Location;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.List;
+import java.util.Properties;
 
 /**
  * Created by student on 4/27/17.
  */
 public class googlemaps {
 
-    Location location = new Location();
-
-    public ??? makeMap(List<Location> locations){
-        //TODO might not be an int
-        List <Integer> location_coordinates;
-
-        for (Location loc : locations) {
-            location_coordinates.add(loc.getCoordinates());
+    //TODO Load properties in its own method
+    Properties prop = new Properties();
+    InputStream input = null;
+	try {
+        input = new FileInputStream("properties.properties");
+        //load a properties file
+        prop.load(input);
+        String center = prop.getProperty("center");
+        String zoomSize = prop.getProperty("zoomSize");
+        String key = prop.getProperty("key");
+        String googleMapsStaticEndpoint = prop.getProperty("googleMappsStaticEndPoint");
+    } catch (IOException ex) {
+        ex.printStackTrace();
+    } finally {
+        if (input != null) {
+            try {
+                input.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+    }
+
+
+    Location location = new Location();
+	//TODO makeMap cannot be void
+    public void makeMap(List<Location> locations){
+
+        createLocationsString(locations);
 
         //TODO make URL parts in properties file
-        InputStream in = new URL(googleMappsEndPoint + location_coordinates).openStream();
+        String parameters = center + zoomSize + markers + key;
+
+        String googleMappsStaticEndPoint = "https://maps.googleapis.com/maps/api/staticmap?";
+
+        InputStream in = new URL(googleMappsStaticEndPoint + parameters).openStream();
+    }
+
+    public String createLocationsString(List <Location> locations){
+        String urlLocations = "";
+        for (Location location : locations){
+            String label = location.getName();
+            String address = location.getAddress();
+            address.replace(' ', '+');
+            urlLocations = urlLocations + "&markers=color:red%7Clabel:" + label + "%7C" + address;
+        }
+        return urlLocations;
     }
 
 }
